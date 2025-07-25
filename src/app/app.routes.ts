f62b56985dcf346f54d9b0cc0eaea6d3cdff8d6a
Neo Zhen Ting <neozt99@gmail.com>
2025-07-25 16:19:27 +0800

    added skeleton for dashboard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,15 @@
 import { Routes } from '@angular/router';
 import { Home } from './features/home/home';
+import { Dashboard } from './features/dashboard/pages/dashboard/dashboard';
 
 export const routes: Routes = [
   {
     path: "",
     component: Home
+  },
+  {
+    path: "dashboard",
+    component: Dashboard
   },
   {
     path: "**",
